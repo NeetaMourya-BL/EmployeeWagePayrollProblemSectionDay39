5d1313de96f83937a06db370ae9e1047524c35fb
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-17 09:11:26 +0530

    [neeta mourya] calculate month wage in UC4 branch

--- Employee_Payroll.js
+++ Employee_Payroll.js
@@ -30,8 +30,16 @@ if (empCheck == IS_ABSENT) {
                 return 0;
         }
     }
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    let empWage = empHrs * WAGE_PER_HOUR;
-    console.log(" Emp Hour:" + empHrs + " Emp Wage: " + empWage);
+   /**
+ * UC4 Calculating wages for a month
+ */
+{
+    let totalEmpHrs = 0;
+    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage);
+}
 }
