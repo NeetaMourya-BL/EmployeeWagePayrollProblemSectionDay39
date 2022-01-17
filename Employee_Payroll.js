7745f00ccef570475eab4604f45db675820b295e
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-01-17 09:07:49 +0530

    [neeta mourya] calculate daily wage hour in UC3 branch

--- Employee_Payroll.js
+++ Employee_Payroll.js
@@ -12,27 +12,26 @@ if (empCheck == IS_ABSENT) {
 
 /**
 * UC2 Calculating Employee Wage
-*/
-{
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
-const PART_TIME_HOURS = 4;
-const FULL_TIME_HOURS = 8;
-const WAGE_PER_HOUR = 20;
+*/{
+    const IS_PART_TIME = 1;
+    const IS_FULL_TIME = 2;
+    const PART_TIME_HOURS = 4;
+    const FULL_TIME_HOURS = 8;
+    const WAGE_PER_HOUR = 20;
+// UC3 write function for daily working hours
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
-}
-
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("Employee Wage:" + empWage);
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log(" Emp Hour:" + empHrs + " Emp Wage: " + empWage);
 }
